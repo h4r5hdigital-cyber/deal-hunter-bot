d052944a0d8c792c6426299c232977200eef9c06
h4r5hdigital-cyber <h4r5h.digital@gmail.com>
2026-06-10 12:49:34 +0530

    Update deal_hunter.py

--- deal_hunter.py
+++ deal_hunter.py
@@ -45,10 +45,12 @@ AMAZON_HEADERS = {
     "Connection": "keep-alive"
 }
 
-# Flipkart ke liye alag mask
+# The Googlebot VIP Mask for Flipkart
 FLIPKART_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive"
 }
 
 # === AMAZON SCRAPER ENGINE ===
